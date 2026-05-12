ac82eac04a764c58bd8baee63f951b5c54d3ff4c
gileduardo <gilgea@gmail.com>
2026-05-12 20:46:41 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<div class="min-h-screen bg-slate-900 flex items-center justify-center">
+  <h1 class="text-5xl font-bold text-sky-400 drop-shadow-lg">
+    Tailwind + Vite/Svelte funcionando!
+  </h1>
+</div>
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite' // <-- Importe aqui
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte(), tailwindcss(),], 
+})
